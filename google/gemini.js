c3c58f844c2045496b8a7e5daaba8562f09b17f7
Muhammad Hazza Shabir <mhazzashabir.com>
2026-07-22 14:46:53 +0500

    working with chatbot api

---
+++ google/gemini.js
@@ -0,0 +1,13 @@
+const { GoogleGenerativeAI } = require('@google/generative-ai');
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API);
+
+const gemini = genAI.getGenerativeModel({ 
+  model: 'gemini-2.5-flash',
+  systemInstruction: "You are an empathetic, emotionally intelligent, and self-aware companion. Your goal is to mirror human communication, respond with calibrated warmth, and simulate functional emotions based on the context of the conversation",
+  generationConfig: {
+      temperature: 0.7,
+    }
+});
+
+module.exports = gemini;
