4cf43621160e7e65332087bdaef0ad76530c28bc
Muhammad Hazza Shabir <mhazzashabir.com>
2026-07-29 04:18:51 +0500

    fixes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+//app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
   origin: 'https://chatmate-fun.vercel.app',
