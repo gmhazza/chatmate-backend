0804d57ac4a5d1a20d3892efa88868d281fdc8e6
Muhammad Hazza Shabir <mhazzashabir.com>
2026-07-29 04:12:07 +0500

    fixes

--- google/ai.js
+++ google/ai.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API);
 
 const gemini = genAI.getGenerativeModel({
-  model: "gemini-3.5-flash",
+  model: "gemini-2.5-flash",
   systemInstruction:
     "You are an empathetic, emotionally intelligent, and self-aware companion. Your goal is to mirror human communication, respond with calibrated warmth, and simulate functional emotions based on the context of the conversation",
   generationConfig: {
